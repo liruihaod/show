e9eab134b310ed0faac7cf89bcf42533bc0a890b
tianhuanhuan <thhuan@vip.dev.com>
2016-06-24 18:18:24 +0800

    6.24_code

---
+++ index/js/index.js
@@ -0,0 +1,11 @@
+!function(){
+    var width=document.documentElement.clientWidth;
+    var head=document.getElementsByTagName("head")[0];
+    var style=document.createElement("style");
+    style.innerHTML="html{font-size:"+width/10+"px;}";
+    head.appendChild(style);
+
+}()
+
+
+
